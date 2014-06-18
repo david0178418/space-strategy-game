df91493ea55d084bd8b453cd7a70e06d77155235
David Granado <davidjgranado@gmail.com>
2014-06-17 20:27:59 -0500

    Use personal fork of phaser

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ requirejs.config({
 	//urlArgs: "bust=" +  (new Date()).getTime(),	//cache-bust if needed
 	baseUrl: 'src',
 	paths: {
-		Phaser: '../libs/phaser-official/build/phaser',
+		Phaser: '../libs/phaser/build/phaser',
 		lodash: '../libs/lodash/dist/lodash'
 	},
 	map: {
