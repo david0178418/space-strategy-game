42bf5380916ce26aadaa1a55959d596f364b5c50
David Granado <davidjgranado@gmail.com>
2014-06-18 23:10:31 -0500

    Zoom in and out of world while maintaining view relative to center point.

--- src/states/play.js
+++ src/states/play.js
@@ -15,10 +15,11 @@ define(function(require) {
 	var PanSpeed = 8,
 		MinSize = 0.25,
 		MaxSize = 3,
-		ScaleIncrement = 0.25;
+		ScaleIncrement = 0.05;
 	
 	States.Play = 'play';
 	game.state.add(States.Play, {
+		_zoomSpeed: 25,	//% per second
 		_zoomTween: null,
 		_zoomTarget: 1,
 		
@@ -34,33 +35,33 @@ define(function(require) {
 			window.worldEntities = this.worldEntities = instanceManager.get('worldEntities');
 			
 			window.addEventListener('mousewheel', _.bind(function(e) {
-				this._zoomTarget += ScaleIncrement * (e.wheelDelta > 0 ? 1 : -1);
-				
-				if(this._zoomTarget < MinSize || this._zoomTarget > MaxSize) {
+				if(game.paused) {
 					return;
 				}
 				
-				if(this._zoomTween) {
-					this._zoomTween.stop();
-				}
-				
-				this._zoomTween = game.add.tween(this.worldEntities.scale).to({x:this._zoomTarget, y:this._zoomTarget}, 200);
-				
-				this._zoomTween.start();
+				this._zoomTarget += ScaleIncrement * (e.wheelDelta > 0 ? 1 : -1);
+				this._zoomTarget = Phaser.Math.clamp(this._zoomTarget, 0.25, 2);
 			}, this));
 
 			game.world.setBounds(0, 0, CONFIG.stage.width, CONFIG.stage.height);
 			
 			game.stage.backgroundColor = '#333';
 			
-			this.planet1 = new Planet({x: 500, y: 500});
-			this.planet2 = new Planet({x: 400, y: 600});
+			this.planet1 = new Planet({x: 0, y: 0});
+			this.planet2 = new Planet({x: 0, y: CONFIG.stage.height / 2});
 			
 			this.ship1 = new Ship({x: 100, y: 100});
 			this.ship2 = new Ship({x: 150, y:150});
 			
 			this.worldEntities.add(this.planet1);
 			this.worldEntities.add(this.planet2);
+			this.worldEntities.add(new Planet({x: CONFIG.stage.width / 2, y: CONFIG.stage.height / 2}));
+			this.worldEntities.add(new Planet({x: CONFIG.stage.width / 2, y: 0}));
+			this.worldEntities.add(new Planet({x: CONFIG.stage.width, y: CONFIG.stage.height / 2}));
+			this.worldEntities.add(new Planet({x: CONFIG.stage.width / 2, y: CONFIG.stage.height}));
+			this.worldEntities.add(new Planet({x: 0, y: CONFIG.stage.height}));
+			this.worldEntities.add(new Planet({x: CONFIG.stage.width, y: 0}));
+			this.worldEntities.add(new Planet({x: CONFIG.stage.width, y: CONFIG.stage.height}));
 			this.worldEntities.add(this.ship1);
 			this.worldEntities.add(this.ship2);
 			
@@ -68,21 +69,33 @@ define(function(require) {
 			this.controls = instanceManager.get('controls');
 		},
 		update: function(game) {
-			
+			if(this.worldEntities.scale.x !== this._zoomTarget) {
+				this.deltaTime = game.time.elapsed;
+				this.updateZoom();
+			}
 			var controls = this.controls;
 			
 			if(controls.panUp.isDown) {
-				this.game.camera.y -= PanSpeed;
+				game.camera.y -= PanSpeed;
 			} else if(controls.panDown.isDown) {
-				this.game.camera.y += PanSpeed;
+				game.camera.y += PanSpeed;
 			}
 			
 			if(controls.panRight.isDown) {
-				this.game.camera.x += PanSpeed;
+				game.camera.x += PanSpeed;
 			} else if(controls.panLeft.isDown) {
-				this.game.camera.x -= PanSpeed;
+				game.camera.x -= PanSpeed;
 			}
 		},
+		
+		updateZoom: function() {
+			//TODO figure out math to transition smoothly
+			this.worldEntities.scale.x = this.worldEntities.scale.y = this._zoomTarget;
+			
+			this.worldEntities.x = this.game.world.width * ( 1 - this._zoomTarget) * 0.5;
+			this.worldEntities.y = this.game.world.height * ( 1 - this._zoomTarget) * 0.5;
+		},
+		
 		paused: function() {
 		},
 	});
